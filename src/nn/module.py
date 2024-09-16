4daaaa71c2deba9695c5040050180c457f33c1af
Paul Ilioaica <paulilioaica@microsoft.com>
2024-09-16 16:33:50 +0300

    Fix param method

--- src/nn/module.py
+++ src/nn/module.py
@@ -5,11 +5,13 @@ class Module:
     def __call__(self, *args):
         raise NotImplementedError
 
-    def parameters(self):
-            param_list = []
-            for attr in self.__dict__.values():
-                if isinstance(attr, Module):
-                    param_list.extend(attr.parameters())
-                elif hasattr(attr, 'parameters'):
-                    param_list.extend(attr.parameters())
-            return param_list
+def parameters(self):
+        param_list = []
+        for attr in self.__dict__.values():
+            if isinstance(attr, Module):
+                param_list.extend(attr.parameters())
+            elif hasattr(attr, '__iter__'):
+                for item in attr:
+                    if hasattr(item, 'parameters'):
+                        param_list.extend(item.parameters())
+        return param_list
