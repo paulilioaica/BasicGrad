e8a800feec344633c4fadf883d1fcaa1a6bdcba0
Paul Ilioaica <paulilioaica@microsoft.com>
2024-09-19 17:25:24 +0300

    Add CategoricalCrossEntropy loss

--- src/loss_functions/cce.py
+++ src/loss_functions/cce.py
@@ -1,24 +1,22 @@
 from loss_functions.loss import LossFunction
+import math
 
-
-class MSELoss(LossFunction):
+class CategoricalCrossEntropy(LossFunction):
     def __init__(self) -> None:
         self.loss = 0
-        self.diff_sum = []
         self.y_pred = []
+        self.y_true = []
 
     def __call__(self, y_pred, y_true):
         self.y_pred = y_pred
+        self.y_true = y_true
         batch_size = len(y_pred)
         batch_loss = 0
-        self.diff_sum = []
 
         for batch in range(batch_size):
-            differences = [self.y_pred[batch][i].value - y_true[batch][i].value for i in range(len(y_pred[batch]))]
-            self.diff_sum.append(differences)
-            loss = sum([i**2 for i in differences])
-            batch_loss += loss
-
+            for i in range(len(y_pred[batch])):
+                loss = -y_true[batch][i].value * math.log(y_pred[batch][i].value)
+                batch_loss += loss
 
         self.loss = batch_loss / batch_size
         return self
@@ -27,8 +25,8 @@ class MSELoss(LossFunction):
         batch_size = len(self.y_pred)
         for batch in range(batch_size):
             for i in range(len(self.y_pred[batch])):
-                self.y_pred[batch][i].grad += 2 * self.diff_sum[batch][i] / batch_size
+                self.y_pred[batch][i].grad += -self.y_true[batch][i].value / self.y_pred[batch][i].value / batch_size
                 self.y_pred[batch][i].backward()
-        
+
     def __repr__(self):
         return f"Loss: {self.loss}"
--- src/nn/linear_layer.py
+++ src/nn/linear_layer.py
@@ -7,15 +7,13 @@ class LinearLayer(Module):
     def __init__(self, input_size, output_size, dropout=False):
         self.input_size = input_size
         self.output_size = output_size
-        self.weight = [[Variable(value=random.random(), _name=f"weight_{i}_{j}") for i in range(self.input_size)] 
-                       for j in range(self.output_size)]
-        self.bias = [Variable(value=random.random(), _name=f"bias_{i}") for i in range(self.output_size)]
+        self.weight = Variable([[random.random() for i in range(self.input_size)] for j in range(self.output_size)])
+        self.bias = Variable([random.random() for i in range(self.output_size)])
 
     def __call__(self, input):
         batch_size = len(input)
 
-        output = [[Variable(value=0, _name=f'output_{batch}_{i}') for i in range(self.output_size)] 
-                       for batch in range(batch_size)]
+        output = Variable([[0 for i in range(self.output_size)] for batch in range(batch_size)])
 
         for batch in range(batch_size):
             for i in range(self.output_size):
